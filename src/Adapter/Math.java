67e0177266cea57355894860137d971f59c7f29a
computer <computer@DESKTOP-SF4T6MK>
2022-03-20 22:19:58 +0900

    프로젝트 변경 어댑터 패턴 시작
    코로나 확진.. 쉬었다 하자

---
+++ src/Adapter/Math.java
@@ -0,0 +1,19 @@
+package Adapter;
+
+// 기존의 정의된 기능들이 있는 클래스
+public class Math {
+	// 두배
+	public static double twoTime(double num) {
+		return num * 2;
+	}
+	
+	// 절반
+	public static double half(double num) {
+		return num / 2;
+	}
+	
+	// 강화된 알고리즘
+	public static Double doubled(Double d) {
+		return d * 2;
+	}
+}
